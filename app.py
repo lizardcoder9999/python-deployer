5d92599761ae3b427adfd0099cdce195127df09b
lizardcoder9999 <lizardcoder6@gmail.com>
2021-02-06 16:30:37 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+import os 
+import sys 
+import base64
+import paramiko
+from cli.cli import Server
+
+print("This currently works for servers running Ubuntu and Debian based linux distrobutions")
+print("Select one of the options below")
+print("(1) Node Js")
+
+
+user_selected = input("Select one of the options above: ")
+converted_selected = str(user_selected)
+
+try:
+    if converted_selected == "1":
+        print("(1) Web App")
+        print("(2) Api")
+
+        project_type = input("Selection: ")
+        converted_project = str(project_type)
+
+    else : 
+        print("You need to choose an option")
+
+    if converted_project == "1":
+        serverObject = Server()
+        serverObject.get_host()
+        serverObject.get_user()
+        serverObject.get_password()
+        client = SSHClient()
+        client.connect(serverObject.host, serverObject.user, serverObject.password)
+        
+
+     
+except: 
+    print("An Error has occured please try again")
+    
+    
---
+++ cli/cli.py
@@ -0,0 +1,19 @@
+class Server:
+    def __init__(self):
+        self.question = "Enter your server info"
+        self.host = ""
+        self.user = ""
+        self.password = ""
+
+        print("Please Enter your server info")
+
+
+    def get_host(self):
+        self.host = str(input("Please enter the host of your server: "))
+
+    def get_user(self):
+        self.user = str(input("Enter the username for your server: "))
+
+    def get_password(self):
+        self.password = str(input("Enter the password for your server: "))
+        
