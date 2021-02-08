801033427c4f39093fcd1c14ac3cac268003da1f
lizardcoder9999 <lizardcoder6@gmail.com>
2021-02-07 23:10:37 -0500

    Started working on the sftp connection

--- node_dep/node.py
+++ node_dep/node.py
@@ -1,5 +1,6 @@
 import paramiko
 import os
+import pysftp
 
 class Node: 
     def __init__(self,server,path=""):
@@ -21,23 +22,18 @@ class Node:
         host,port = self.server.host,port
         transport = paramiko.Transport((host,port))
         username,password = self.server.user,self.server.password
-        transport.connect(None,username,password)
-        sftp = paramiko.SFTPClient.from_transport(transport)
-
         makedir_sh_dir = os.path.dirname(os.path.realpath('__file__'))
         makedir_sh_name = os.path.join(makedir_sh_dir,"scripts/jsnode/makedir.sh")
+        with pysftp.Connection('hostname', username=username, password=password) as sftp:
+            with sftp.mkdir(self.server.project_name,mode=777)
+
+
+       
         
 
         #Upload
-        # filepath = f"{self.path}/{self.server.project}"
         script_server_path = f"{self.path}/scripts"
-        # localpath = self.server.project_path
-        # sftp.put(localpath,filepath)
-        sftp.mkdir("scripts",777)
-        sftp.put(makedir_sh_name,script_server_path)
 
-        if sftp: sftp.close()
-        if transport: transport.close()
 
     def make_dir(self):
         makedir_sh_dir = os.path.dirname(os.path.realpath('__file__'))
@@ -45,15 +41,7 @@ class Node:
         with open(makedir_sh_name, 'w') as f:
             data = f"sudo su \n{self.server.user}\n{self.server.password}\nmkdir {self.server.project}\nchmod -R 777 {self.server.project}"
             f.write(data)
-     
-
-
-        # self.session.exec_command("sudo su",get_pty=True)
-        # self.session.exec_command(self.server.user,get_pty=True)
-        # self.session.exec_command(self.server.password,get_pty=True)
-        # self.session.exec_command(f"mkdir {self.server.project}",get_pty=True)
-        # self.session.exec_command(f"chmod -R 777 {self.server.project}",get_pty=True)
-        # self.session.exec_command(f"cd {self.server.project}",get_pty=True)
+    
         (stdin, stdout, stderr) = self.session.exec_command("pwd",get_pty=True)
         for line in stdout.read().splitlines() :
             print(line)
